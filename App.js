bc32acc1559ae69d9eb8375dca86b7e8444f9808
thisis-Solomon <solomonnjobvu1@gmail.com>
2021-04-27 01:22:53 +0200

    my portifolio template

---
+++ App.js
@@ -0,0 +1,29 @@
+const hamburger = document.querySelector(
+    ".header .nav-bar .nav-list .hamburger"
+);
+const mobileMenu = document.querySelector(".header .nav-bar .nav-list ul");
+const mobileMenuItem = document.querySelectorAll(
+    ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header .container");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+    var scrollPosition = window.scrollY;
+    if (scrollPosition > 250) {
+        header.style.backgroundColor = "#29323c";
+    } else {
+        header.style.backgroundColor = "transparent";
+    }
+});
+
+mobileMenuItem.forEach((item) => {
+    item.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        mobileMenu.classList.toggle("active");
+    });
+});
